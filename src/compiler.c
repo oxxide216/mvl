e5363632a876c871e3b7a45f5ea8231f5e7a4528
vie <vie.devr@gmail.com>
2025-05-21 11:31:47 +0300

    Added procedurs overloading and improved error messages

--- src/compiler.c
+++ src/compiler.c
@@ -8,25 +8,138 @@
 #include "shl_log.h"
 #include "parser.h"
 
+typedef Da(ValueKind) ParamKinds;
+
 typedef struct {
-  Program   *program;
-  Parser     parser;
-  u32        static_segments_count;
+  Str       var;
+  ValueKind kind;
+} VariableKind;
+
+typedef struct {
+  Da(VariableKind) kinds;
+  Da(VariableKind) static_kinds;
+} VariableKinds;
+
+typedef struct {
+  Str        name;
+  ValueKind  ret_val_kind;
+  ParamKinds param_kinds;
+} DefinedProcedure;
+
+typedef Da(DefinedProcedure) DefinedProcedures;
+
+typedef struct {
+  Program            *program;
+  Parser              parser;
+  VariableKinds       var_kinds;
+  DefinedProcedures   procs;
+  u32                 static_segments_count;
 } Compiler;
+
+static Str value_kind_to_str(ValueKind kind) {
+  switch (kind) {
+  case ValueKindUnit: return STR_LIT("unit");
+  case ValueKindS64:  return STR_LIT("s64");
+  case ValueKindS32:  return STR_LIT("s32");
+  case ValueKindS16:  return STR_LIT("s16");
+  case ValueKindS8:   return STR_LIT("s8");
+
+  default: {
+    ERROR("Wrong value kind\n");
+    exit(1);
+  }
+  }
+}
+
+static u32 define_proc(Compiler *compiler, Str name,
+                       ValueKind ret_val_kind,
+                       ParamKinds param_kinds) {
+  DefinedProcedure proc = { name, ret_val_kind, param_kinds };
+  DA_APPEND(compiler->procs, proc);
+  return compiler->procs.len - 1;
+}
+
+static void fprint_proc_sign(FILE *stream, Str name, ParamKinds param_kinds) {
+  str_fprint(stream, name);
+  for (u32 i = 0; i < param_kinds.len; ++i) {
+    fputc(' ', stream);
+    Str kind_str = value_kind_to_str(param_kinds.items[i]);
+    str_fprint(stream, kind_str);
+  }
+}
+
+static bool proc_sign_eq(Str a_name, ParamKinds a_param_kinds,
+                         Str b_name, ParamKinds b_param_kinds) {
+  if (!str_eq(a_name, b_name))
+    return false;
+
+  if (a_param_kinds.len != b_param_kinds.len)
+    return false;
+
+  for (u32 i = 0; i < a_param_kinds.len; ++i)
+    if (a_param_kinds.items[i] != b_param_kinds.items[i])
+      return false;
+
+  return true;
+}
+
+static u32 get_proc_id(Compiler *compiler, Str name,
+                       ParamKinds param_kinds,
+                       u32 current_proc_id) {
+  for (u32 i = 0; i < compiler->procs.len; ++i) {
+    DefinedProcedure *proc = compiler->procs.items + i;
+    if (proc_sign_eq(proc->name, proc->param_kinds, name, param_kinds))
+      return i;
+  }
+
+  DefinedProcedure *current_proc = compiler->procs.items + current_proc_id;
+
+  ERROR("");
+  fprint_proc_sign(stderr, current_proc->name,
+                   current_proc->param_kinds);
+  fputs(": procedure `", stderr);
+  fprint_proc_sign(stderr, name, param_kinds);
+  fputs("` was not found\n", stderr);
+  exit(1);
+}
+
+// djb2 hash function
+static Str get_proc_hashed_name(Compiler *compiler, u32 id) {
+  DefinedProcedure *proc = compiler->procs.items + id;
+  u32 hash = 5381;
+
+  for (u32 i = 0; i < proc->param_kinds.len; ++i)
+    hash = ((hash << 5) + hash) + proc->param_kinds.items[i];
+
+  StringBuilder sb = {0};
+  sb_push_str(&sb, proc->name);
+  sb_push_char(&sb, '@');
+  sb_push_u32(&sb, hash);
+
+  return sb_to_str(sb);
+}
+
+static ValueKind get_proc_ret_val_kind(Compiler *compiler, u32 id) {
+  return compiler->procs.items[id].ret_val_kind;
+}
 
 static Arg token_to_arg(Token *token, Compiler *compiler) {
   if (token->id == TT_STR_LIT) {
     StringBuilder sb = {0};
     sb_push(&sb, "?s");
     sb_push_u32(&sb, compiler->static_segments_count++);
+    Str name = sb_to_str(sb);
 
     u8 *bytes = aalloc(sizeof(token->lexeme.len) + 1);
     memcpy(bytes, token->lexeme.ptr, token->lexeme.len);
     bytes[token->lexeme.len] = '\0';
-    program_push_static_segment(compiler->program, sb_to_str(sb),
+    program_push_static_segment(compiler->program, name,
                                 bytes, token->lexeme.len + 1);
 
-    return arg_var(sb_to_str(sb));
+    VariableKind var_kind = { name, ValueKindS64 };
+    DA_APPEND(compiler->var_kinds.kinds, var_kind);
+
+    return arg_var(name);
   }
 
   if (token->id == TT_CHAR_LIT) {
@@ -40,6 +153,37 @@ static Arg token_to_arg(Token *token, Compiler *compiler) {
   return str_to_arg(token->lexeme, arg_kind);
 }
 
+ValueKind compiler_get_arg_kind(Compiler *compiler, Arg *arg) {
+  switch (arg->kind) {
+  case ArgKindValue: {
+    return arg->as.value.kind;
+  }
+
+  case ArgKindVar: {
+    for (u32 i = 0; i < compiler->var_kinds.kinds.len; ++i) {
+      VariableKind *var_kind = compiler->var_kinds.kinds.items + i;
+      if (str_eq(var_kind->var, arg->as.var))
+        return var_kind->kind;
+    }
+
+    for (u32 i = 0; i < compiler->var_kinds.static_kinds.len; ++i) {
+      VariableKind *var_kind = compiler->var_kinds.static_kinds.items + i;
+      if (str_eq(var_kind->var, arg->as.var))
+        return var_kind->kind;
+    }
+
+    ERROR("Variable `"STR_FMT"` was not defined before usage\n",
+          STR_ARG(arg->as.var));
+    exit(1);
+  } break;
+
+  default: {
+    ERROR("Wrong argument kind\n");
+    exit(1);
+  }
+  }
+}
+
 static Arg compile_arg(Compiler *compiler) {
   Token *arg = parser_expect_token(&compiler->parser, MASK(TT_IDENT) |
                                                       MASK(TT_NUMBER) |
@@ -50,20 +194,78 @@ static Arg compile_arg(Compiler *compiler) {
   return token_to_arg(arg, compiler);
 }
 
-static void compile_call(Compiler *compiler, Procedure *proc, Str dest) {
-  Token *callee = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+static void compile_call(Compiler *compiler, Str dest,
+                         Procedure *current_proc, u32 caller_id) {
+  Token *callee_name_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
   Args args = {0};
+  ParamKinds param_kinds = {0};
 
   while (parser_peek_token(&compiler->parser) &&
          parser_peek_token(&compiler->parser)->id != TT_NEWLINE) {
     Arg arg = compile_arg(compiler);
     args_push_arg(&args, arg);
+
+    ValueKind param_kind = compiler_get_arg_kind(compiler, &arg);
+    DA_APPEND(param_kinds, param_kind);
   }
 
-  if (dest.len > 0)
-    proc_call_assign(proc, dest, callee->lexeme, args);
-  else
-    proc_call(proc, callee->lexeme, args);
+  u32 callee_id = get_proc_id(compiler, callee_name_token->lexeme,
+                              param_kinds, caller_id);
+  Str callee_name = get_proc_hashed_name(compiler, callee_id);
+
+  if (dest.len > 0) {
+    ValueKind param_kind = get_proc_ret_val_kind(compiler, callee_id);
+    VariableKind var_kind = { dest, param_kind };
+    DA_APPEND(compiler->var_kinds.kinds, var_kind);
+
+    proc_call_assign(current_proc, dest, callee_name, args);
+  } else {
+    proc_call(current_proc, callee_name, args);
+  }
+}
+
+void collect_procs_and_static_vars(Compiler *compiler) {
+  define_proc(compiler, STR_LIT("init"), ValueKindUnit, (ParamKinds) {0});
+
+  while (parser_peek_token(&compiler->parser)) {
+    Token *token = parser_next_token(&compiler->parser);
+
+    if (token->id == TT_PROC) {
+      Token *name_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+      ValueKind ret_val_kind = ValueKindUnit;
+      ParamKinds param_kinds = {0};
+
+      Token *next = parser_peek_token(&compiler->parser);
+      while (next && next->id != TT_NEWLINE && next->id != TT_RIGHT_ARROW) {
+        parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+        parser_expect_token(&compiler->parser, MASK(TT_COLON));
+        Token *param_kind_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+
+        ValueKind param_kind = str_to_value_kind(param_kind_token->lexeme);
+        DA_APPEND(param_kinds, param_kind);
+
+        next = parser_peek_token(&compiler->parser);
+      }
+
+      if (next && next->id == TT_RIGHT_ARROW) {
+        parser_next_token(&compiler->parser);
+        Token *ret_val_kind_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+        ret_val_kind = str_to_value_kind(ret_val_kind_token->lexeme);
+      }
+
+      define_proc(compiler, name_token->lexeme, ret_val_kind, param_kinds);
+    } else if (token->id == TT_STATIC) {
+      Token *name_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+      parser_expect_token(&compiler->parser, MASK(TT_COLON));
+      Token *kind_token = parser_expect_token(&compiler->parser, MASK(TT_IDENT));
+
+      ValueKind value_kind = str_to_value(kind_token->lexeme).kind;
+      VariableKind var_kind = { name_token->lexeme, value_kind };
+      DA_APPEND(compiler->var_kinds.static_kinds, var_kind);
+    }
+  }
+
+  compiler->parser.index = 0;
 }
 
 void compile(Tokens tokens, Program *program) {
@@ -71,8 +273,13 @@ void compile(Tokens tokens, Program *program) {
   compiler.program = program;
   compiler.parser.tokens = tokens;
 
-  Procedure *proc = program_push_proc(program, STR_LIT("init"),
+  Procedure *proc = program_push_proc(program, STR_LIT("@init"),
                                       ValueKindUnit, (ProcParams) {0});
+
+  u32 current_proc_id = define_proc(&compiler, STR_LIT("@init"),
+                                    ValueKindUnit, (ParamKinds) {0});
+
+  collect_procs_and_static_vars(&compiler);
 
   while (parser_peek_token(&compiler.parser)) {
     Token *token = parser_expect_token(&compiler.parser, MASK(TT_NEWLINE) |
@@ -104,20 +311,26 @@ void compile(Tokens tokens, Program *program) {
     }
 
     case TT_PROC: {
-      Token *name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
+      Token *name_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
       ValueKind ret_val_kind = ValueKindUnit;
       ProcParams params = {0};
+      ParamKinds param_kinds = {0};
+
+      compiler.var_kinds.kinds.len = 0;
 
       Token *next = parser_peek_token(&compiler.parser);
       while (next && next->id != TT_NEWLINE && next->id != TT_RIGHT_ARROW) {
-        Token *param_name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
+        Token *param_name_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
         parser_expect_token(&compiler.parser, MASK(TT_COLON));
-        Token *param_kind = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
+        Token *param_kind_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
 
-        ProcParam proc_param = {
-          param_name->lexeme,
-          str_to_value(param_kind->lexeme).kind,
-        };
+        ValueKind param_kind = str_to_value_kind(param_kind_token->lexeme);
+        DA_APPEND(param_kinds, param_kind);
+
+        VariableKind var_kind = { param_name_token->lexeme, param_kind };
+        DA_APPEND(compiler.var_kinds.kinds, var_kind);
+
+        ProcParam proc_param = { param_name_token->lexeme, param_kind };
         DA_APPEND(params, proc_param);
 
         next = parser_peek_token(&compiler.parser);
@@ -126,10 +339,13 @@ void compile(Tokens tokens, Program *program) {
       if (next && next->id == TT_RIGHT_ARROW) {
         parser_next_token(&compiler.parser);
         Token *ret_val_kind_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
-        ret_val_kind = str_to_value(ret_val_kind_token->lexeme).kind;
+        ret_val_kind = str_to_value_kind(ret_val_kind_token->lexeme);
       }
 
-      proc = program_push_proc(program, name->lexeme, ret_val_kind, params);
+      current_proc_id = define_proc(&compiler, name_token->lexeme,
+                                    ret_val_kind, param_kinds);
+      Str name = get_proc_hashed_name(&compiler, current_proc_id);
+      proc = program_push_proc(program, name, ret_val_kind, params);
     } break;
 
     case TT_JUMP: {
@@ -192,11 +408,14 @@ void compile(Tokens tokens, Program *program) {
 
       if (next && next->id == TT_AT) {
         parser_next_token(&compiler.parser);
-        compile_call(&compiler, proc, token->lexeme);
+        compile_call(&compiler, token->lexeme, proc, current_proc_id);
         break;
       }
 
       if (next && next->id == TT_REF) {
+        VariableKind var_kind = { token->lexeme, ValueKindS64 };
+        DA_APPEND(compiler.var_kinds.kinds, var_kind);
+
         parser_next_token(&compiler.parser);
         Token *src_name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
         proc_ref(proc, token->lexeme, src_name->lexeme);
@@ -209,6 +428,10 @@ void compile(Tokens tokens, Program *program) {
         Token *ref_name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
 
         ValueKind kind = str_to_value_kind(value_kind_name->lexeme);
+
+        VariableKind var_kind = { token->lexeme, kind };
+        DA_APPEND(compiler.var_kinds.kinds, var_kind);
+
         proc_deref(proc, token->lexeme, kind, ref_name->lexeme);
         break;
       }
@@ -219,16 +442,25 @@ void compile(Tokens tokens, Program *program) {
         Token *var_name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
 
         ValueKind kind = str_to_value_kind(value_kind_name->lexeme);
+
+        VariableKind var_kind = { token->lexeme, kind };
+        DA_APPEND(compiler.var_kinds.kinds, var_kind);
+
         proc_cast(proc, token->lexeme, kind, var_name->lexeme);
         break;
       }
 
       Arg arg = compile_arg(&compiler);
+
+      ValueKind param_kind = compiler_get_arg_kind(&compiler, &arg);
+      VariableKind var_kind = { token->lexeme, param_kind };
+      DA_APPEND(compiler.var_kinds.kinds, var_kind);
+
       proc_assign(proc, token->lexeme, arg);
     } break;
 
     case TT_AT: {
-      compile_call(&compiler, proc, (Str) {0});
+      compile_call(&compiler, (Str) {0}, proc, current_proc_id);
     } break;
 
     case TT_INCLUDE: {
@@ -236,12 +468,14 @@ void compile(Tokens tokens, Program *program) {
     } break;
 
     case TT_STATIC: {
-      Token *name = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
+      Token *name_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
       parser_expect_token(&compiler.parser, MASK(TT_COLON));
-      Token *kind = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
+      Token *kind_token = parser_expect_token(&compiler.parser, MASK(TT_IDENT));
 
-      Value value = { str_to_value(kind->lexeme).kind, {0} };
-      program_push_static_var(program, name->lexeme, value);
+      ValueKind value_kind = str_to_value(kind_token->lexeme).kind;
+
+      Value value = { value_kind, {0} };
+      program_push_static_var(program, name_token->lexeme, value);
     } break;
 
     case TT_ASM: {
